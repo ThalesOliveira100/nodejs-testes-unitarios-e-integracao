e574b72897ab907d1b823568a1a0d65ea14f599f
Thales Oliveira <sr.thales500@gmail.com>
2025-10-16 17:01:03 -0300

    feat: add initial project setup with package.json and Jest for testing
    - Created package.json with project metadata and development dependencies including Jest and ESLint.
    - Added test file for folhaPagamento with unit tests for somaHorasExtras and calculaDescontos functions.

---
+++ test/folhaPagamento.test.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-undef */
+import { somaHorasExtras, calculaDescontos } from "../index.js";
+
+describe("CT001 - Testes dos cálculos de folha", () => {
+
+    it("Deve retornar a soma das horas extras", () => {
+        const esperado = 2500;
+        const retornado = somaHorasExtras(2000, 500);
+
+        expect(retornado).toBe(esperado);
+    });
+
+    it("Deve descontar o valor do salário", () => {
+        const esperado = 2200; 
+        const retornado = calculaDescontos(2500, 300);
+
+        expect(retornado).toBe(esperado);
+    });
+});
+
